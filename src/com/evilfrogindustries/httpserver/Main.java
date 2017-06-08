2b6cffdba80e4579adda935e8a4c80fc7261340f
BuildTools <areyouagitated@gmail.com>
2017-06-07 22:37:06 -0400

    Working before substatinal mod

--- src/com/evilfrogindustries/httpserver/Main.java
+++ src/com/evilfrogindustries/httpserver/Main.java
@@ -2,6 +2,7 @@ package com.evilfrogindustries.httpserver;
 
 import com.evilfrogindustries.httpserver.context.IndexContext;
 import com.sun.net.httpserver.HttpServer;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -13,13 +14,34 @@ public class Main {
     private HttpServer server;
 
     public static void main(String[] args) {
-        Main main = new Main();
-        main.startServer();
+        new Main().run();
     }
 
-    public void startServer() {
+    private void run() {
+        startServer();
+        makeResourceDirectory();
+    }
+
+    /*
+    If I set the context to /index.html, it only reads the index.html file, and not
+    any of the link files in it. If i set the context to "/" and navigate to /index.html
+    in my browser, it works. I'm not sure why.
+    */
+    private void startServer() {
+        try {
+            server = HttpServer.create(new InetSocketAddress(80), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        server.createContext("/", new IndexContext());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    private void makeResourceDirectory() {
         //If the resources directory does not exist, create it.
-        Path contentDirectory = Paths.get("./src/Resources");
+        Path contentDirectory = Paths.get("./Resources");
         if (!Files.exists(contentDirectory)) {
             System.out.println("Content path does not exist. Creating.");
             try {
@@ -29,21 +51,6 @@ public class Main {
                 System.out.println("Could not make the content path.");
             }
         }
-
-        try {
-            server = HttpServer.create(new InetSocketAddress(80), 0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        /*If I set the context to /index.html, it only reads the index.html file, and not
-        any of the link files in it. If i set the context to "/" and navigate to /index.html
-        in my browser, it works. I'm not sure why.
-         */
-        //Add the possible contexts the server can handle
-        server.createContext("/", new IndexContext());
-        server.setExecutor(null); // creates a default executor
-        server.start();
     }
 
 }
--- src/com/evilfrogindustries/httpserver/context/IndexContext.java
+++ src/com/evilfrogindustries/httpserver/context/IndexContext.java
@@ -3,14 +3,11 @@ package com.evilfrogindustries.httpserver.context;
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
+import java.io.*;
+import java.net.URI;
+
+//TODO see why an exception is thrown, send proper file-type
 public class IndexContext implements HttpHandler {
 
     @Override
@@ -21,54 +18,101 @@ public class IndexContext implements HttpHandler {
 
         //Test to see if the index should be sent.
         if (uriObject.toString().equals("/") || uriObject.toString().equals("\\")) {
+            uriObject = URI.create(httpExchange.getRequestURI().toString() + "index.html");
             try {
-                uriObject = URI.create(httpExchange.getRequestURI().toString() + "index.html");
-
                 //Get the data for the file requested by the path.
-                File test = new File("./src/Resources" + uriObject.getPath());
-                Path indexPath = Paths.get(test.getCanonicalPath());
-                byte[] b = Files.readAllBytes(indexPath);
+                File resource = new File("./Resources" + uriObject.getPath());
+                byte[] b = createByteArray(resource);
 
-                //Outputs standard header information.
-                Headers header = httpExchange.getResponseHeaders();
-                header.add("Content-Type", "text/html");
-                httpExchange.sendResponseHeaders(200, b.length);
+                //Outputs standard header information and index body.
+                writeHttpHeader(httpExchange, b, "text/html");
+                writeHttpBody(httpExchange.getResponseBody(), b);
 
-                //Writes to the http body
-                OutputStream responseStream = httpExchange.getResponseBody();
-                responseStream.write(b);
             } catch (Exception e) {
-                httpExchange.sendResponseHeaders(404, 0);
-                String error = "<h1>Put the index file in /Resources/.</h1>";
-                byte[] b = error.getBytes();
-                httpExchange.getResponseBody().write(b);
+                e.printStackTrace();
+                sendResponse404(httpExchange, "<h1>Put the index file in /Resources/.</h1>");
             } finally {
                 httpExchange.getResponseBody().close();
             }
         }
+
         //Handles sending all files besides the index.
-        //TODO send codes for different file types.
         try {
             //Read all files index.html needs.
-            File test = new File("./src/Resources" + httpExchange.getRequestURI().getPath());
-            Path indexPath = Paths.get(test.getCanonicalPath());
-            byte[] b = Files.readAllBytes(indexPath);
+            File test = new File("./Resources" + httpExchange.getRequestURI().getPath());
+            byte[] b = createByteArray(test);
 
+            //Send the response header and body.
             httpExchange.sendResponseHeaders(200, b.length);
+            writeHttpBody(httpExchange.getResponseBody(), b);
 
-            //Writes to the http body
-            OutputStream responseStream = httpExchange.getResponseBody();
-            responseStream.write(b);
         } catch (Exception e) {
-            httpExchange.sendResponseHeaders(404, 0);
-            String error = "<h1>404 not found.</h1>";
-            byte[] b = error.getBytes();
-            httpExchange.getResponseBody().write(b);
+            e.printStackTrace();
+            sendResponse404(httpExchange, "<h1>404 Not Found.</h1>");
+
         } finally {
             httpExchange.getResponseBody().close();
         }
 
     }
 
+    //Methods that drive the main context.
+
+    public void writeHttpHeader(HttpExchange httpExchange, byte[] b, String text) throws IOException {
+        Headers header = httpExchange.getResponseHeaders();
+        header.add("Content-Type", text);
+        httpExchange.sendResponseHeaders(200, b.length);
+    }
+
+    public void writeHttpBody(OutputStream responseStream, byte[] b) throws IOException {
+        responseStream = new BufferedOutputStream(responseStream);
+        responseStream.write(b, 0, b.length);
+        responseStream.flush();
+    }
+
+    public void sendResponse404(HttpExchange httpExchange, String errorMessage) {
+        try {
+            httpExchange.sendResponseHeaders(404, 0);
+            byte[] b = errorMessage.getBytes();
+            writeHttpBody(httpExchange.getResponseBody(), b);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] createByteArray(File file) throws IOException{
+        InputStream in = null;
+        ByteArrayOutputStream ous = null;
+        try {
+            byte[] buffer = new byte[4096];
+            in = new BufferedInputStream(new FileInputStream(file));
+            ous = new ByteArrayOutputStream();
+            int read = 0;
+
+            //Use the buffered InputStream so that the method keeps reading until the buffer is full.
+            while ((read = in.read(buffer, 0, buffer.length)) != -1) {
+                ous.write(buffer, 0, read);
+            }
+
+        }finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Problems closing the file we are reading from.");
+            }
+
+            try {
+                if(ous != null) {
+                    ous.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Problems closing the output stream to create the byte array.");
+            }
+        }
+        return ous.toByteArray();
+    }
 }
 
